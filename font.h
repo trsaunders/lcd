6ca56b6ab9d871d41afc101868a89b0e6df4561e
Thomas Saunders <trsaunders@gmail.com>
2012-01-04 22:16:50 +0000

    Very basic functionality, including 3 fonts

---
+++ font.h
@@ -0,0 +1,13 @@
+#ifndef __FONT_H__
+#define __FONT_H__
+
+#include "ch.h"
+#include "hal.h"
+
+#define LCD_USE_FONTS 1
+
+#define LCD_USE_FONT_SMALL 1
+#define LCD_USE_FONT_MEDIUM 1
+#define LCD_USE_FONT_LARGE 1
+
+#endif

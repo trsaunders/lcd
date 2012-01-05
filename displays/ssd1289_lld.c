9d13821b33208d75573c12f8d154edaba8446f7d
Thomas Saunders <trsaunders@gmail.com>
2012-01-05 09:53:28 +0000

    removed some other panel code from ssd1289_lld.c

--- displays/ssd1289_lld.c
+++ displays/ssd1289_lld.c
@@ -46,18 +46,9 @@ static __inline uint16_t LCD_ReadRAM(void) {
 }
 
 static void lcd_set_cursor_lld(uint16_t Xpos,uint16_t Ypos) {
-	if(DeviceCode==0x8989) {
-		LCD_WriteReg(0x004e,Xpos); /* Row */
-		LCD_WriteReg(0x004f,Ypos); /* Line */
-	} else if(DeviceCode==0x9919) {
-		LCD_WriteReg(0x004e,Xpos); /* Row */
-		LCD_WriteReg(0x004f,Ypos); /* Line */
-	} else {
-		LCD_WriteReg(0x0020,Xpos); /* Row */
-		LCD_WriteReg(0x0021,Ypos); /* Line */
-	}
+	LCD_WriteReg(0x004e,Xpos);
+	LCD_WriteReg(0x004f,Ypos);
 }
-
 
 void lcd_set_area_lld(uint16_t x0,uint16_t y0,uint16_t x1,uint16_t y1) {
 	lcd_set_cursor_lld(x0, y0);
@@ -95,57 +86,49 @@ void lcd_init_lld(void) {
 	/* Bank1 NOR/SRAM control register configuration */
 	FSMC_Bank1->BTCR[FSMC_Bank] = FSMC_BCR1_MWID_0 | FSMC_BCR1_WREN | FSMC_BCR1_MBKEN;
 
-	DeviceCode = LCD_ReadReg(0x0000);
+	LCD_WriteReg(SSD_OSC_START, SSD_OSCEN);    SSD1289_Delay(5);
+	LCD_WriteReg(SSD_PWR_CNTRL_1,0xA8A4);    SSD1289_Delay(5);
+	LCD_WriteReg(SSD_PWR_CNTRL_2,0x0000);    SSD1289_Delay(5);
+	LCD_WriteReg(SSD_PWR_CNTRL_3,0x080C);    SSD1289_Delay(5);
+	LCD_WriteReg(SSD_PWR_CNTRL_4,0x2B00);    SSD1289_Delay(5);
+	LCD_WriteReg(SSD_PWR_CNTRL_5,0x00B0);    SSD1289_Delay(5);
+	LCD_WriteReg(SSD_DRV_OUT_CNTRL,0x2B3F);    SSD1289_Delay(5);
+	LCD_WriteReg(SSD_LCD_DRV_AC_CNTRL,0x0600);    SSD1289_Delay(5);
+	LCD_WriteReg(SSD_SLEEP_MODE,0x0000);    SSD1289_Delay(5);
+	LCD_WriteReg(SSD_ENTRY_MODE,0x6070);    SSD1289_Delay(5);
+	LCD_WriteReg(SSD_CMP_REG_1,0x0000);    SSD1289_Delay(5);
+	LCD_WriteReg(SSD_CMP_REG_2,0x0000);    SSD1289_Delay(5);
+	LCD_WriteReg(SSD_HORZ_PORCH,0xEF1C);    SSD1289_Delay(5);
+	LCD_WriteReg(SSD_VERT_PORCH,0x0003);    SSD1289_Delay(5);
+	LCD_WriteReg(SSD_DISP_CNTRL,0x0133);    SSD1289_Delay(5);
+	LCD_WriteReg(SSD_FRAME_CYC_CNTRL,0x0000);    SSD1289_Delay(5);
+	LCD_WriteReg(SSD_GATE_SCAN_START,0x0000);    SSD1289_Delay(5);
+	LCD_WriteReg(SSD_VERT_SCROLL_1,0x0000);    SSD1289_Delay(5);
+	LCD_WriteReg(SSD_VERT_SCROLL_2,0x0000);    SSD1289_Delay(5);
+	LCD_WriteReg(SSD_FIRST_WIN_START,0x0000);    SSD1289_Delay(5);
+	LCD_WriteReg(SSD_FIRST_WIN_END,0x013F);    SSD1289_Delay(5);
+	LCD_WriteReg(SSD_SECOND_WIN_START,0x0000);    SSD1289_Delay(5);
+	LCD_WriteReg(SSD_SECOND_WIN_END,0x0000);    SSD1289_Delay(5);
+	LCD_WriteReg(SSD_HORZ_RAM_ADDR,0xEF00);    SSD1289_Delay(5);
+	LCD_WriteReg(SSD_VERT_RAM_START,0x0000);    SSD1289_Delay(5);
+	LCD_WriteReg(SSD_VERT_RAM_END,0x013F);    SSD1289_Delay(5);
+	/* y control */
+	LCD_WriteReg(0x0030,0x0707);    SSD1289_Delay(5);
+	LCD_WriteReg(0x0031,0x0204);    SSD1289_Delay(5);
+	LCD_WriteReg(0x0032,0x0204);    SSD1289_Delay(5);
+	LCD_WriteReg(0x0033,0x0502);    SSD1289_Delay(5);
+	LCD_WriteReg(0x0034,0x0507);    SSD1289_Delay(5);
+	LCD_WriteReg(0x0035,0x0204);    SSD1289_Delay(5);
+	LCD_WriteReg(0x0036,0x0204);    SSD1289_Delay(5);
+	LCD_WriteReg(0x0037,0x0502);    SSD1289_Delay(5);
+	LCD_WriteReg(0x003A,0x0302);    SSD1289_Delay(5);
+	LCD_WriteReg(0x003B,0x0302);    SSD1289_Delay(5);
 
-	/* initialise LCD */
-	if(DeviceCode == 0x8989) {
-		LCD_WriteReg(SSD_OSC_START, SSD_OSCEN);    SSD1289_Delay(5);
-		LCD_WriteReg(SSD_PWR_CNTRL_1,0xA8A4);    SSD1289_Delay(5);
-		LCD_WriteReg(SSD_PWR_CNTRL_2,0x0000);    SSD1289_Delay(5);
-		LCD_WriteReg(SSD_PWR_CNTRL_3,0x080C);    SSD1289_Delay(5);
-		LCD_WriteReg(SSD_PWR_CNTRL_4,0x2B00);    SSD1289_Delay(5);
-		LCD_WriteReg(SSD_PWR_CNTRL_5,0x00B0);    SSD1289_Delay(5);
-		LCD_WriteReg(SSD_DRV_OUT_CNTRL,0x2B3F);    SSD1289_Delay(5);
-		LCD_WriteReg(SSD_LCD_DRV_AC_CNTRL,0x0600);    SSD1289_Delay(5);
-		LCD_WriteReg(SSD_SLEEP_MODE,0x0000);    SSD1289_Delay(5);
-		LCD_WriteReg(SSD_ENTRY_MODE,0x6070);    SSD1289_Delay(5);
-		LCD_WriteReg(SSD_CMP_REG_1,0x0000);    SSD1289_Delay(5);
-		LCD_WriteReg(SSD_CMP_REG_2,0x0000);    SSD1289_Delay(5);
-		LCD_WriteReg(SSD_HORZ_PORCH,0xEF1C);    SSD1289_Delay(5);
-		LCD_WriteReg(SSD_VERT_PORCH,0x0003);    SSD1289_Delay(5);
-		LCD_WriteReg(SSD_DISP_CNTRL,0x0133);    SSD1289_Delay(5);
-		LCD_WriteReg(SSD_FRAME_CYC_CNTRL,0x0000);    SSD1289_Delay(5);
-		LCD_WriteReg(SSD_GATE_SCAN_START,0x0000);    SSD1289_Delay(5);
-		LCD_WriteReg(SSD_VERT_SCROLL_1,0x0000);    SSD1289_Delay(5);
-		LCD_WriteReg(SSD_VERT_SCROLL_2,0x0000);    SSD1289_Delay(5);
-		LCD_WriteReg(SSD_FIRST_WIN_START,0x0000);    SSD1289_Delay(5);
-		LCD_WriteReg(SSD_FIRST_WIN_END,0x013F);    SSD1289_Delay(5);
-		LCD_WriteReg(SSD_SECOND_WIN_START,0x0000);    SSD1289_Delay(5);
-		LCD_WriteReg(SSD_SECOND_WIN_END,0x0000);    SSD1289_Delay(5);
-		LCD_WriteReg(SSD_HORZ_RAM_ADDR,0xEF00);    SSD1289_Delay(5);
-		LCD_WriteReg(SSD_VERT_RAM_START,0x0000);    SSD1289_Delay(5);
-		LCD_WriteReg(SSD_VERT_RAM_END,0x013F);    SSD1289_Delay(5);
-		/* y control */
-		LCD_WriteReg(0x0030,0x0707);    SSD1289_Delay(5);
-		LCD_WriteReg(0x0031,0x0204);    SSD1289_Delay(5);
-		LCD_WriteReg(0x0032,0x0204);    SSD1289_Delay(5);
-		LCD_WriteReg(0x0033,0x0502);    SSD1289_Delay(5);
-		LCD_WriteReg(0x0034,0x0507);    SSD1289_Delay(5);
-		LCD_WriteReg(0x0035,0x0204);    SSD1289_Delay(5);
-		LCD_WriteReg(0x0036,0x0204);    SSD1289_Delay(5);
-		LCD_WriteReg(0x0037,0x0502);    SSD1289_Delay(5);
-		LCD_WriteReg(0x003A,0x0302);    SSD1289_Delay(5);
-		LCD_WriteReg(0x003B,0x0302);    SSD1289_Delay(5);
-
-		LCD_WriteReg(SSD_RAM_WRITE_MASK_1,0x0000);    SSD1289_Delay(5);
-		LCD_WriteReg(SSD_RAM_WRITE_MASK_2,0x0000);    SSD1289_Delay(5);
-		LCD_WriteReg(SSD_FRAME_FREQ,0x8000);    SSD1289_Delay(5);
-		LCD_WriteReg(SSD_GDDRAM_Y,0);
-		LCD_WriteReg(SSD_GDDRAM_X,0);
-	} else {
-		chSysHalt();
-	}
-	/* TODO: add other devices from original code */
+	LCD_WriteReg(SSD_RAM_WRITE_MASK_1,0x0000);    SSD1289_Delay(5);
+	LCD_WriteReg(SSD_RAM_WRITE_MASK_2,0x0000);    SSD1289_Delay(5);
+	LCD_WriteReg(SSD_FRAME_FREQ,0x8000);    SSD1289_Delay(5);
+	LCD_WriteReg(SSD_GDDRAM_Y,0);
+	LCD_WriteReg(SSD_GDDRAM_X,0);
 }
 
 void lcd_set_pixel_lld(uint16_t x, uint16_t y, uint16_t colour) {
